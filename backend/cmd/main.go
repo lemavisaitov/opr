3b234d6a799419ee0698d616a7df0facb6a84bde
Lema Visaitov <l.visaitov@bi.zone>
2025-05-11 07:18:20 +0300

    add cmd, yara rules mock and dockerfile

---
+++ backend/cmd/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/lemavisaitov/opr/internal/app"
+	"github.com/lemavisaitov/opr/internal/handler"
+	"github.com/lemavisaitov/opr/internal/usecase"
+	"github.com/lemavisaitov/opr/pkg/logger"
+	"github.com/lemavisaitov/opr/tool/yarascanner"
+	"go.uber.org/zap"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const (
+	YaraRulesDir = "./yara_rules"
+)
+
+func main() {
+	err := logger.Init("Debug")
+	if err != nil {
+		panic(err)
+	}
+
+	scanner, err := yarascanner.New(YaraRulesDir)
+	if err != nil {
+		logger.Fatal("Failed to create yara scanner",
+			zap.Error(err),
+		)
+	}
+	yaraUC := usecase.New(scanner)
+	handler := handler.NewHandler(yaraUC)
+	router := app.NewRouter(handler)
+	server := http.Server{
+		Addr:              ":8080",
+		Handler:           router,
+		ReadHeaderTimeout: 60 * time.Second,
+	}
+
+	shutdownChan := make(chan os.Signal, 1)
+	signal.Notify(shutdownChan, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-shutdownChan
+	}()
+
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Fatal("Failed to start server",
+			zap.Error(err),
+		)
+	}
+}
--- backend/go.mod
+++ backend/go.mod
@@ -1,2 +1,16 @@
 module github.com/lemavisaitov/opr
+
 go 1.23.0
+
+require (
+	github.com/go-chi/chi/v5 v5.2.1
+	github.com/pkg/errors v0.9.1
+	go.uber.org/zap v1.27.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
+)
+
+require (
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/go-chi/render v1.0.3 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+)
